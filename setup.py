7658a265fdfd0bb6f7a29d3f5a0ee008bff2f23c
Claudia <claudialclarke@aol.com>
2014-12-27 22:02:21 +0000

    Added back script to setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "Greenspace",
     version = "0.1",
     packages = find_packages(exclude=['*test']),
-    
+    scripts = ['command.py'],
     #scripts = ['greenspace/command'],
     #scripts = ['scripts/greenspace'],
     install_requires = ['argparse']
