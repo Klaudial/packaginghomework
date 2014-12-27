24648e2c1dfd8848f610f8a29f828f34622ee0ae
Claudia <claudialclarke@aol.com>
2014-12-27 21:33:52 +0000

    Removed script from setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "Greenspace",
     version = "0.1",
     packages = find_packages(exclude=['*test']),
-    scripts = ['greenspace/command'],
+    
     #scripts = ['greenspace/command'],
     #scripts = ['scripts/greenspace'],
     install_requires = ['argparse']
